77efcebd566f0f9a4ca594ab750056069341378b
nazennash <nazennash120@gmail.com>
2024-11-06 11:49:23 +0300

    header-image

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -2,12 +2,12 @@ import image from "../../assets/imag1.jpg";
 
 export const Header = () => {
   return (
-    <div className="m-5 lg:m-0">
+    <div className="m-2 lg:m-0">
       <div className="relative w-full h-auto flex ">
         <img
           src={image}
           alt="Food"
-          className="w-full md:h-96 object-cover rounded-xl "
+          className="w-full h-60 md:h-96 object-cover rounded-xl "
         />
         <div className="absolute inset-0 p-5 flex flex-col gap-5 justify-center bg-gray-900 bg-opacity-50 rounded-xl  text-white">
           <div className="flex flex-col gap-3 md:gap-5 max-w-lg">
@@ -16,9 +16,7 @@ export const Header = () => {
             </p>
             <p className="text-sm md:text-base">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              laboriosam pariatur, saepe accusamus blanditiis et. Porro, impedit
-              odit. Fuga esse tempore ullam, omnis eos vero facilis delectus
-              nostrum assumenda tempora.
+              laboriosam pariatur, saepe accusamus blanditiis et.
             </p>
           </div>
           <button className="w-32 md:w-40 bg-white text-black font-semibold py-2 rounded-3xl hover:bg-gray-200 transition duration-300">
